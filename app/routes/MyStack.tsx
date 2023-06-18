4e16ab91a85753734341271e3fe1de9462d7b9c4
Levan Samadashvili <levan.samadashvili@azry.com>
2023-06-18 18:56:25 +0400

    add logo to header

--- app/routes/MyStack.tsx
+++ app/routes/MyStack.tsx
@@ -1,13 +1,27 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/Home";
 import About from "../screens/About";
+import { Image } from "react-native";
 
 const Stack = createNativeStackNavigator();
+
+const LogoTitle = () => {
+  return (
+    <Image
+      style={{ width: 50, height: 50 }}
+      source={require("../assets/favicon.png")}
+    />
+  );
+};
 
 const MyStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ headerTitle: () => <LogoTitle /> }}
+      />
       <Stack.Screen name="About" component={About} />
     </Stack.Navigator>
   );
