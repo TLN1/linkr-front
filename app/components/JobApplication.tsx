680fd9826e8bb1336cd1f75176d3c8dc4bb79102
ninikhundzakishvili <nkhun19@freeuni.edu.ge>
2023-09-01 02:17:50 +0400

    patches not working

--- app/components/JobApplication.tsx
+++ app/components/JobApplication.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { useState, useRef } from "react";
+import { View, StyleSheet, Text, KeyboardAvoidingView } from "react-native";
 import DropDownPicker from "react-native-dropdown-picker";
+import { RichText, Bold, Italic, OrderedList, UnorderedList, Link, Media } from "react-native-rte";
+
 
 export const JobApplication = () => {
-
   const [jobLocationOpen, setJobLocationOpen] = useState(false);
   const [jobLocation, setJobLocation] = useState("");
 
@@ -13,10 +14,46 @@ export const JobApplication = () => {
   const [experienceLevelOpen, setExperienceLevelOpen] = useState(false);
   const [experienceLevel, setExperienceLevel] = useState("");
 
+  const [tagPickerState, setTagPickerState] = useState({
+    tags: {
+      tag: "",
+      tagsArray: [],
+    },
+  });
+
+  const [description, setDescription] = useState("");
+
+  interface TagState {
+    tag: string;
+    tagsArray: string[];
+  }
+
+  const updateTagPickerState = (state: TagState) => {
+    setTagPickerState({
+      tags: state,
+    });
+  };
+
+  const [skills, setSkills] = useState([]);
+
+  const richText = useRef();
+
+  const [descHTML, setDescHTML] = useState("");
+  const [showDescError, setShowDescError] = useState(false);
+
+  const richTextHandle = (descriptionText) => {
+    if (descriptionText) {
+      setShowDescError(false);
+      setDescHTML(descriptionText);
+    } else {
+      setShowDescError(true);
+      setDescHTML("");
+    }
+  };
+
   return (
     <View style={styles.dialogContainer}>
       <View style={styles.dialogFrame}>
-
         <View style={{ padding: 10, marginLeft: "4%" }}>
           <Text style={{ fontSize: 20, fontWeight: "bold" }}>
             Add job application
@@ -24,8 +61,8 @@ export const JobApplication = () => {
         </View>
 
         <DropDownPicker
-          //   style={styles.dropdownone}
-          //   containerStyle={styles.dropdownContainerStyleOne}
+          style={styles.dropdown}
+          containerStyle={styles.dropdownContainerStyle}
           open={jobLocationOpen}
           value={jobLocation}
           items={[]}
@@ -35,8 +72,8 @@ export const JobApplication = () => {
           //   setItems={setIndustryItems}
         />
         <DropDownPicker
-          //   style={styles.dropdownone}
-          //   containerStyle={styles.dropdownContainerStyleOne}
+          style={styles.dropdown}
+          containerStyle={styles.dropdownContainerStyle}
           open={jobTypeOpen}
           value={jobType}
           items={[]}
@@ -47,8 +84,8 @@ export const JobApplication = () => {
         />
 
         <DropDownPicker
-          //   style={styles.dropdownone}
-          //   containerStyle={styles.dropdownContainerStyleOne}
+          style={styles.dropdown}
+          containerStyle={styles.dropdownContainerStyle}
           open={experienceLevelOpen}
           value={experienceLevel}
           items={[]}
@@ -57,12 +94,51 @@ export const JobApplication = () => {
           placeholder="Select experience level"
           //   setItems={setIndustryItems}
         />
+
+        {/* <View style={styles.richTextContainer}>
+          <RichEditor
+            ref={richText}
+            onChange={richTextHandle}
+            placeholder="Write your cool content here :)"
+            androidHardwareAccelerationDisabled={true}
+            style={styles.richTextEditorStyle}
+            initialHeight={250}
+          />
+          <RichToolbar
+            editor={richText}
+            selectedIconTint="#873c1e"
+            iconTint="#312921"
+            actions={[
+              actions.setBold,
+              actions.setItalic,
+              actions.insertBulletsList,
+              actions.insertOrderedList,
+              actions.insertLink,
+              actions.setStrikethrough,
+              actions.setUnderline,
+            ]}
+            style={styles.richTextToolbarStyle}
+          />
+        </View> */}
+        <KeyboardAvoidingView style={{flex:1}} behavior={'padding'}>
+          <RichText>
+            <RichText.Editor />
+            <RichText.Toolbar>
+              <Bold />
+              <Italic />
+              <OrderedList />
+              <UnorderedList />
+              {/* <Link onPress={this.addLink.bind(this)} /> */}
+              {/* <Media onPress={this.selectMedia.bind(this)} /> */}
+            </RichText.Toolbar>
+          </RichText>
+          </KeyboardAvoidingView>
       </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   dialogContainer: {
     backgroundColor: "transparent", // Set to transparent to make the frame visible
     flex: 1,
@@ -76,6 +152,59 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 20,
     elevation: 5,
+  },
+  dropdown: {
+    // marginTop: 12,
+    // marginBottom: 12,
+    // position: "relative",
+    // zIndex:1,
+    // borderWidth: 0,
+    backgroundColor: "#fff",
+    borderColor: "#bbb",
+    // padding: 10,
+    // border:10,
+    marginBottom: 15,
+    shadowColor: "#000000",
+  },
+  dropdownContainerStyle: {
+    zIndex: 2,
+    marginTop: 10,
+    marginBottom: 10,
+    borderColor: "#bbb",
+    borderRadius: 5,
+    position: "relative",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  richTextContainer: {
+    display: "flex",
+    flexDirection: "column-reverse",
+    width: "100%",
+    marginBottom: 10,
+  },
+
+  richTextEditorStyle: {
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    borderWidth: 1,
+    borderColor: "#ccaf9b",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+    fontSize: 20,
+  },
+
+  richTextToolbarStyle: {
+    backgroundColor: "#c6c3b3",
+    borderColor: "#c6c3b3",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderWidth: 1,
   },
 });
 
