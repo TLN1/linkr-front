85a80f42d8c555df4d512b01b30ef5e372f48416
ninikhundzakishvili <nkhun19@freeuni.edu.ge>
2023-09-02 03:11:28 +0400

    add safe area

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
 import MainNavigator from "./routes/MainNavigator";
+import {
+  SafeAreaProvider,
+} from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <MainNavigator />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <MainNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
