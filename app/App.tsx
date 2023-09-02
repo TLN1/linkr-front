d6cbc2b010ea3d9b6eb1d18ede104d73f8500185
tmike19 <tmike19@freeuni.edu.ge>
2023-09-03 00:25:15 +0400

    add safearea provider in app

--- app/App.tsx
+++ app/App.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import MainNavigator from "./routes/MainNavigator";
 import { ToastProvider } from "react-native-toast-notifications";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <ToastProvider>
+      <SafeAreaProvider>
       <NavigationContainer>
         <MainNavigator />
       </NavigationContainer>
+      </SafeAreaProvider>
     </ToastProvider>
   );
 }
