a48cdcb602f96fd0e5eadf6de4c18c3c8c1a194e
ninikhundzakishvili <nkhun19@freeuni.edu.ge>
2023-09-03 18:47:32 +0400

    style job app view modal

--- app/components/JobApplicationView.tsx
+++ app/components/JobApplicationView.tsx
@@ -1,38 +1,122 @@
-import { View, ScrollView, Text } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  View,
+  ScrollView,
+  Text,
+  StyleSheet,
+  Dimensions,
+  SafeAreaView,
+} from "react-native";
+import { get } from "../axios";
+import { Ionicons } from "@expo/vector-icons";
+import Unorderedlist from "react-native-unordered-list";
 
 export const JobApplicationView = (
-  title: string,
-  experienceLevel: string,
-  jobLocation: string,
-  jobType: string,
-  skills: string[],
-  description: string
+  application_id: any,
+  isVisible: any,
+  setVisible: any
 ) => {
+  const [title, setTitle] = useState("");
+  const [experienceLevel, setExperienceLevel] = useState("");
+  const [jobLocation, setJobLocation] = useState("");
+  const [jobType, setJobType] = useState("");
+  const [skills, setSkills] = useState([]);
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    async function fetchApplication(application_id: number) {
+      // Fetch data
+      const { data } = await get(`/application/${application_id}`);
+      if (data?.location) {
+        const location = data?.location;
+        setJobLocation(location);
+      }
+
+      if (data?.job_type) {
+        const type = data?.job_type;
+        setJobType(type);
+      }
+
+      setTitle(data?.title);
+      setExperienceLevel(data?.experience_level);
+      setDescription(data?.description);
+      setSkills(data?.skills);
+    }
+
+    if (application_id) fetchApplication(application_id);
+  }, [application_id]);
+
   return (
-    <View>
-      <ScrollView>
-        <Text style={{ fontSize: 15, padding: 8 }}>Job title:</Text>
-        <Text style={{ fontSize: 10, padding: 5 }}>{title}</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.dialogContainer}>
+        <View style={styles.dialogFrame}>
+          <ScrollView>
+            <View
+              style={{
+                padding: 10,
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{alignContent:'center', textAlignVertical: 'center',fontSize: 20, fontWeight: "bold" }}>{title}</Text>
+              <Ionicons.Button
+                name="close"
+                size={24}
+                color="black"
+                backgroundColor="white"
+                style={{
+                  alignContent: "center",
+                  justifyContent: "center",
+                  flex: 1,
+                  alignSelf: "center",
+                  paddingEnd: 0,
+                }}
+                onPress={() => setVisible(false)}
+              />
+            </View>
+            <Text style={{ fontSize: 20, padding: 8, color: 'grey', fontWeight:'bold' }}>Experience level:</Text>
+            <Text style={{ fontSize: 15 , padding: 8, paddingTop: 0}}>{experienceLevel}</Text>
 
-        <Text style={{ fontSize: 15, padding: 8 }}>Experience level:</Text>
-        <Text style={{ fontSize: 10, padding: 5 }}>{experienceLevel}</Text>
+            <Text style={{ fontSize: 20, padding: 8, color: 'grey', fontWeight:'bold' }}>Job location:</Text>
+            <Text style={{ fontSize: 15, padding: 8 }}>{jobLocation}</Text>
 
+            <Text style={{ fontSize: 20, padding: 8, color: 'grey', fontWeight:'bold' }}>Job type:</Text>
+            <Text style={{ fontSize: 15, padding: 8 }}>{jobType}</Text>
 
-        <Text style={{ fontSize: 15, padding: 8 }}>Job location:</Text>
-        <Text style={{ fontSize: 10, padding: 5 }}>{jobLocation}</Text>
-
-        <Text style={{ fontSize: 15, padding: 8 }}>Job type:</Text>
-        <Text style={{ fontSize: 10, padding: 5 }}>{jobType}</Text>
-
-        <Text style={{ fontSize: 15, padding: 8 }}>Skills:</Text>
-        <View style={{flexDirection: 'row'}}>
-            {skills.map((skill) => <Text style={{backgroundColor:'grey', color: 'black'}}>{skill}</Text>)}
+            <Text style={{ fontSize: 20, padding: 8, color: 'grey', fontWeight:'bold' }}>Skills:</Text>
+            <View>
+              {skills.map((skill) => (
+                <View key={skill} style={{ padding: 5 }}>
+                  <Unorderedlist bulletUnicode={0x2023}>
+                    <Text style={{ fontSize: 15 }}>{skill}</Text>
+                  </Unorderedlist>
+                </View>
+              ))}
+            </View>
+            <View>
+              <Text style={{ fontSize: 20, padding: 8, color: 'grey', fontWeight:'bold' }}>Description:</Text>
+              <Text style={{ fontSize: 15, padding: 8 }}>{description}</Text>
+            </View>
+          </ScrollView>
         </View>
-        <View>
-          <Text style={{ fontSize: 15, padding: 8 }}>Description:</Text>
-          <Text>{description}</Text>
-        </View>
-      </ScrollView>
-    </View>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  dialogContainer: {
+    padding: 10,
+    marginTop: 100,
+    height: (Dimensions.get("window").height * 2) / 3,
+  },
+  dialogFrame: {
+    backgroundColor: "white",
+    borderWidth: 2,
+    borderColor: "transparent",
+    borderRadius: 10,
+    padding: 20,
+    elevation: 5,
+    width: 300,
+  },
+});
